8cd2b3dc703d5f46e4622aba8bf9e5f7dd192813
ai22m030 <ai22m030@technikum-wien.at>
2023-03-08 03:51:00 +0100

    show all agents play againts each other

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from open_spiel.python import rl_environment
 from open_spiel.python.algorithms import tabular_qlearner
 from open_spiel.python.algorithms import random_agent
 
+env = rl_environment.Environment("tic_tac_toe")
+
+
+def evaluate(a):
+    ts = env.reset()
+    while not ts.last():
+        print("")
+        print(env.get_state)
+        pid = ts.observations["current_player"]
+        # Note the evaluation flag. A Q-learner will set epsilon=0 here.
+        ao = a[pid].step(ts, is_evaluation=True)
+        print(f"Agent {pid} chooses {env.get_state.action_to_string(ao.action)}")
+        ts = env.step([ao.action])
+
+    print("")
+    print(env.get_state)
+    print(ts.rewards)
+
 
 if __name__ == '__main__':
     # Create the environment
-    env = rl_environment.Environment("tic_tac_toe")
     num_players = env.num_players
     num_actions = env.action_spec()["num_actions"]
 
@@ -29,21 +46,23 @@ if __name__ == '__main__':
             agent.step(time_step)
     print("Done!")
 
+    # Random Agents play against each other
+    eval_agents = [random_agent.RandomAgent(0, num_actions, "Entropy Master 2000"),
+                   random_agent.RandomAgent(1, num_actions, "Entropy Master 2000")]
+
+    evaluate(eval_agents)
+
     # Evaluate the Q-learning agent against a random agent.
+    eval_agents = [random_agent.RandomAgent(0, num_actions, "Entropy Master 2000"), agents[1]]
+
+    evaluate(eval_agents)
+
     eval_agents = [agents[0], random_agent.RandomAgent(1, num_actions, "Entropy Master 2000")]
+
+    evaluate(eval_agents)
+
     # Play against each other
-    # eval_agents = [agents[0], agents[1]]
+    eval_agents = [agents[0], agents[1]]
 
-    time_step = env.reset()
-    while not time_step.last():
-        print("")
-        print(env.get_state)
-        player_id = time_step.observations["current_player"]
-        # Note the evaluation flag. A Q-learner will set epsilon=0 here.
-        agent_output = eval_agents[player_id].step(time_step, is_evaluation=True)
-        print(f"Agent {player_id} chooses {env.get_state.action_to_string(agent_output.action)}")
-        time_step = env.step([agent_output.action])
+    evaluate(eval_agents)
 
-    print("")
-    print(env.get_state)
-    print(time_step.rewards)
